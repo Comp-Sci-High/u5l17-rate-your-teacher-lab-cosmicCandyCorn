243c52fb5b412e21326d9a122e8913edd3e93982
Miriam. C <miriam.clemente25@compscihigh.org>
2025-04-10 14:09:30 +0000

    dgd

--- public/ratingScript.js
+++ public/ratingScript.js
@@ -1,2 +1,21 @@
 // Add an event listener to the form that upon submits creates a new rating
 // When the rating is created redirect to the ratings page
+
+
+const form = document.querySelector("form")
+
+form.addEventListener("submit"), async (e)=>{
+    e.preventDefault()
+    const aTeacher = new formData (createform)
+    const reqBody = Object.fromEntries(form)
+    const response = await fetch("/add", {
+  method: "POST",
+  headers: {
+"Content-Type": "application/json"
+},
+body:JSON.Stringify(reqBody)
+})
+const data = await response.json() 
+console.log(data)
+window.location.href = "/"
+}
--- public/teacherScript.js
+++ public/teacherScript.js
@@ -4,20 +4,21 @@
 const { create } = require("domain")
 const { fetch } = require("undici-types")
 
-      
+
 const form = document.querySelector("form")
 
-createform.addEventListener("Submit"), async (e)=>{
+form.addEventListener("submit"), async (e)=>{
     e.preventDefault()
-
     const aTeacher = new formData (createform)
     const reqBody = Object.fromEntries(form)
-
-const response = await fetch("/all"), {
+    const response = await fetch("/add", {
   method: "POST",
-headers: {
-    "Content-Type": "application/json"
+  headers: {
+"Content-Type": "application/json"
 },
-body:
-
+body:JSON.Stringify(reqBody)
+})
+const data = await response.json() 
+console.log(data)
+window.location.href = "/"
 }
