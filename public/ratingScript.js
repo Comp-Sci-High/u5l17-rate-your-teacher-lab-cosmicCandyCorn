604f8cbf02536430306cce093543642e1d154f9d
Miriam. C <miriam.clemente25@compscihigh.org>
2025-04-10 18:15:18 +0000

    dhhd

--- public/ratingScript.js
+++ public/ratingScript.js
@@ -4,18 +4,20 @@
 
 const form = document.querySelector("form")
 
-form.addEventListener("submit"), async (e)=>{
-    e.preventDefault()
-    const aTeacher = new formData (createform)
-    const reqBody = Object.fromEntries(form)
-    const response = await fetch("/add", {
-  method: "POST",
-  headers: {
-"Content-Type": "application/json"
-},
-body:JSON.Stringify(reqBody)
+createForm.addEventListener("submit", async (e) => {
+  e.preventDefault()
+  const ratingg = new formData(createForm)
+  const reqBody = Object.fromEntries(ratingg)
+
+  const response = await fetch("/add/rating", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(reqBody)
+  })
+  const data = await response.json()
+  console.log(data)
+  window.location.href = "/"
 })
-const data = await response.json() 
-console.log(data)
-window.location.href = "/"
-}
+
--- public/teacherScript.js
+++ public/teacherScript.js
@@ -1,24 +1,27 @@
 // Add an event listener to the form that upon submits creates a new teacher
 // When the teacher is created redirect to the teachers page
 
-const { create } = require("domain")
-const { fetch } = require("undici-types")
+
 
 
 const form = document.querySelector("form")
 
-form.addEventListener("submit"), async (e)=>{
-    e.preventDefault()
-    const aTeacher = new formData (createform)
-    const reqBody = Object.fromEntries(form)
-    const response = await fetch("/add", {
-  method: "POST",
-  headers: {
-"Content-Type": "application/json"
-},
-body:JSON.Stringify(reqBody)
+createForm.addEventListener("submit", async (e) => {
+  e.preventDefault()
+  const ratingg = new formData(createForm)
+  const reqBody = Object.fromEntries(ratingg)
+
+  const response = await fetch("/add/teacher", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(reqBody)
+  })
+  const data = await response.json()
+  console.log(data)
+  window.location.href = "/"
 })
-const data = await response.json() 
-console.log(data)
-window.location.href = "/"
-}
+
+
+
